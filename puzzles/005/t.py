3f9d5ca40abe61767b392f7b517ede7c65b715b3
Anthony Sottile <asottile@umich.edu>
2025-03-11 20:05:52 -0400

    better starting point for 005

--- puzzles/005/t.py
+++ puzzles/005/t.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
+from collections.abc import Generator
 
-def flatten(lst):
-    def _helper(lst):
+
+def flatten[T](lst) -> list[T]:
+    def _helper(lst) -> Generator[T]:
         for item in lst:
             if isinstance(item, list):
                 yield from _helper(item)
