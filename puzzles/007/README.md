ce352e4dfc61f060899d7be2a1f467f410d14d14
anthony sottile <asottile@umich.edu>
2025-06-03 20:36:19 -0400

    add puzzle #007

---
+++ puzzles/007/README.md
@@ -0,0 +1,8 @@
+why is this erroring?
+=====================
+
+<details><summary>hint</summary>
+
+try running mypy with `--disallow-any-generics`!
+
+</details>
---
+++ puzzles/007/t.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from typing import Any
+
+
+class User:
+    ...
+
+
+class NAdmin:
+    def get_user_by_id(self, user_id: int) -> User:
+        raise NotImplementedError()
+
+
+class NObject[T]:
+    def __init__(self, n: T) -> None:
+        self._n = n
+
+    def _get_field_value(self, field: str) -> Any:
+        pass
+
+
+class Post(NObject):
+    def __init__(self, n: NAdmin) -> None:
+        super().__init__(n)
+        self._user_id = self._get_field_value('user')
+        self._user = None
+
+    @property
+    def user(self) -> User:
+        if self._user is None:
+            self._user = self._n.get_user_by_id(self._user_id)
+        return self._user
