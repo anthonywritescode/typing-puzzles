1751d3764c98dad5a09847cb8e42fb66d56e99d1
anthony sottile <asottile@umich.edu>
2025-06-03 20:17:09 -0400

    add puzzle 006

---
+++ puzzles/006/README.md
@@ -0,0 +1,9 @@
+sometimes sentinel
+==================
+
+<details><summary>hint</summary>
+
+maybe there's another way to make a singleton the type checker will
+understand? (not `None` -- that might be an acceptable value!)
+
+</details>
---
+++ puzzles/006/t.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+_MISSING = object()
+
+
+def do(x=_MISSING):
+    if x is _MISSING:
+        print('no `x` was passed!')
+    elif x is None:
+        print('`x` was explicitly passed as `None`')
+    else:
+        print(f'x**2 is {x**2}')
+
+
+do()
+do(None)
+do(9001)
+do('no')  # should be an error!
